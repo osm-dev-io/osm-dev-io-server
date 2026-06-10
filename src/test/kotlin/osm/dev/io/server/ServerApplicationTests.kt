456fdb8e0a1534b0918fac9010ffadca5554b438
Seongmin Oh <osm.dev.io@gmail.com>
2026-06-10 22:45:18 +0900

    initial commit

---
+++ src/test/kotlin/osm/dev/io/server/ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package osm.dev.io.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
