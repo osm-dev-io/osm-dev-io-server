ed5ab873a39c6f97ddba3dbfb20455ac879d307e
Seongmin Oh <osm.dev.io@gmail.com>
2026-06-14 23:24:35 +0900

    Post에 createdAt, updatedAt 추가

--- src/main/kotlin/osm/dev/io/server/domain/post/Post.kt
+++ src/main/kotlin/osm/dev/io/server/domain/post/Post.kt
@@ -2,13 +2,19 @@ package osm.dev.io.server.domain.post
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import org.hibernate.annotations.JdbcTypeCode
 import org.hibernate.type.SqlTypes
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
 
+@EntityListeners(AuditingEntityListener::class)
 @Entity
 @Table(name = "posts")
 class Post(
@@ -21,4 +27,11 @@ class Post(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
+
+    @CreatedDate
+    @Column(updatable = false)
+    var createdAt: LocalDateTime? = null
+
+    @LastModifiedDate
+    var updatedAt: LocalDateTime? = null
 }
