28728a4724a12996dee35c2ca8fc1c84c1c73023
Seongmin Oh <osm.dev.io@gmail.com>
2026-06-20 11:22:27 +0900

    로그인 정보 활용 기능 구현

--- src/main/kotlin/osm/dev/io/server/domain/user/controller/UserController.kt
+++ src/main/kotlin/osm/dev/io/server/domain/user/controller/UserController.kt
@@ -1,5 +1,7 @@
 package osm.dev.io.server.domain.user.controller
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,5 +21,12 @@ class UserController(
     ): ApiResponse<Nothing> {
         userService.signup(request.email, request.password)
         return ApiResponse.success()
+    }
+
+    @GetMapping("/me")
+    fun myInfo(
+        @AuthenticationPrincipal email: String
+    ): ApiResponse<String> {
+        return ApiResponse.success(email)
     }
 }
